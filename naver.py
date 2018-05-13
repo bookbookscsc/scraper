abd99dfe1587d7d92d6a8f7e59d4768d7f20459e
minomi <5minhominho@gmail.com>
2018-05-14 01:54:57 +0900

    특정 책의 리뷰링크 가져오기 (예외처리x)

--- naver.py
+++ naver.py
@@ -1,29 +1,36 @@
 from requests_html import HTMLSession
 import re
 
-session = HTMLSession()
 
+class NaverBookScraper:
 
-def get_bids(url):
+    def __init__(self):
+        self.session = HTMLSession()
+        self.headers = {
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Referer': f'www.naver.com',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36',
+        }
 
-    headers = {
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Accept-Language':'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
-        'Referer': f'www.naver.com',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36',
-    }
-
-    def gen_bids(url):
-        resp = session.get(url, headers=headers)
+    def get_bids(self, url):
+        resp = self.session.get(url=url,
+                                headers=self.headers)
         for i, link in enumerate(resp.html.links):
             try:
                 bid = re.search('http:\/\/book.naver.com\/bookdb\/book_detail\.nhn\?bid=(\d+)', link).group(1)
                 yield bid
-            except AttributeError as e:
+            except AttributeError:
                 continue
 
-    return gen_bids(url=url)
+    def get_review_links(self, bid, page=1):
+        url = f'http://book.naver.com/bookdb/review.nhn?bid={bid}&page={page}'
+        resp = self.session.get(url=url,
+                                headers=self.headers)
+        return (review_list.attrs['href'] for review_list in resp.html.xpath("//ul[@id='reviewList']/li//a"))
 
 
-for bid in get_bids(url='http://book.naver.com/bookdb/book_detail.nhn?bid=11211133'):
-    print(bid)
+if __name__ == '__main__':
+    nbs = NaverBookScraper()
+    for review in nbs.get_review_links(bid=13542164, page=2):
+        print(review)
