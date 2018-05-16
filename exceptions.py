00dbbe354b4c834575d7a3f8a27ce16269c54a87
minomi <5minhominho@gmail.com>
2018-05-17 04:13:43 +0900

    부모 Exception 정의

--- exceptions.py
+++ exceptions.py
@@ -1,24 +1,29 @@
+class BookScrapingException(Exception):
+    def __init__(self, provider_name=None, book_name=None):
+        self.provider_name = provider_name
+        self.book_name = book_name
 
-class ISBNNotFoundException(Exception):
+
+class ISBNNotFoundException(BookScrapingException):
     def __str__(self):
         return "페이지에서 ISBN 을 발견하지 못했습니다."
 
 
-class BookLogDetailPopupNotOpenException(Exception):
+class BookLogDetailPopupNotOpenException(BookScrapingException):
     def __str__(self):
         return "북 로그 상세 페이지 팝업을 여는데 실패 했습니다."
 
 
-class NotExistBookLogException(Exception):
+class PaginationException(BookScrapingException):
     def __str__(self):
-        return "북로그 리뷰가 없습니다."
+        return f"{self.book_name}, 올바른 페이지 범위가 아닙니다."
 
 
-class PaginationException(Exception):
+class NotExistReviews(BookScrapingException):
     def __str__(self):
-        return "올바른 페이지 범위가 아닙니다."
+        return f"{self.book_name} 의 리뷰가 없습니다."
 
 
-class FailToGetTotalPageCount(Exception):
+class FailToGetTotalPageCount(BookScrapingException):
     def __str__(self):
-        return "총 리뷰 개수를 구하는데 실패했습니다."
+        return f"{self.book_name} 의 총 리뷰 개수를 구하는데 실패했습니다."
