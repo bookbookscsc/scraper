029ba816627973ec6c130d0d3a8ad1a6668d52c6
minomi <5minhominho@gmail.com>
2018-05-24 16:04:12 +0900

    필요 없는 request 제거, logger setter 제거, 모든 페이지가 아닌 10개 페이지만 scraping 하도록 변경

--- naver.py
+++ naver.py
@@ -1,5 +1,4 @@
 import re
-import math
 import logging
 from urllib3.exceptions import HTTPError
 from requests_html import HTMLSession
@@ -10,8 +9,10 @@ from exceptions import (FailToGetTotalPageCountError,
 
 class NaverBooksScraper:
 
-    def __init__(self):
-        self.session = HTMLSession(mock_browser=False)
+    PAGE_LIMIT = 10
+
+    def __init__(self, logger=None):
+        self.session = HTMLSession()
         self.headers = {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
@@ -22,61 +23,59 @@ class NaverBooksScraper:
                           ' Chrome/66.0.3359.181 Safari/537.36',
             'Connection': 'keep-alive'
         }
-        self.logger = None
+        self.logger = logger
         self.scraping_bid_sets = set()
         self.scraped_bid_sets = set()
 
-    def update_scraping_bids_from(self, url):
-        resp = self.session.get(url=url,
-                                headers=self.headers)
-        for link_in_html in resp.html.links:
+    def update_scraping_bids_in(self, html):
+        for link_in_html in html.links:
             try:
                 bid = re.search('http:\/\/book.naver.com\/bookdb\/book_detail\.nhn\?bid=(\d+)', link_in_html).group(1)
                 if bid not in self.scraped_bid_sets:
                     self.scraping_bid_sets.add(bid)
             except AttributeError:
                 continue
-        del resp
 
-    def gen_review_links(self, bid, page=0):
-        total_page_num = self.total_page_num(bid, page)
+    def gen_review_links(self, bid):
         url = f'http://book.naver.com/bookdb/review.nhn?bid={bid}'
-        self.update_scraping_bids_from(url)
+        response = self.session.get(url, headers=self.headers)
+        html = response.html
+        self.update_scraping_bids_in(html)
+        review_count = self.get_reviews_count(bid, html)
+        self.logger.info(f'{bid} review count is {review_count}')
+        del response
 
-        def gen_reviews_links_per_page(page):
-            resp = self.session.get(url=f'{url}&page={page}', headers=self.headers)
-            if resp.ok:
-                for review_list in resp.html.xpath("//ul[@id='reviewList']/li/dl/dt/a"):
-                    yield review_list.attrs['href']
-            else:
-                raise HTTPError
-            del resp
+        def gen_reviews_links_per_page(cur_page):
+            resp = self.session.get(url=f'{url}&page={cur_page}', headers=self.headers)
+            try:
+                if resp.ok:
+                    for review_list in resp.html.xpath("//ul[@id='reviewList']/li/dl/dt/a"):
+                        yield review_list.attrs['href']
+                else:
+                    raise HTTPError
+            finally:
+                del resp
 
-        for page in range(1, total_page_num + 1):
+        for page in range(1, NaverBooksScraper.PAGE_LIMIT):
             yield from gen_reviews_links_per_page(page)
 
-    def total_page_num(self, bid, page):
-        url = f'http://book.naver.com/bookdb/review.nhn?bid={bid}&page={page}'
-        total_reviews_span = self.session.get(url=url, headers=self.headers).html.xpath("//span[@class='num']")[0]
+    def get_reviews_count(self, bid, html):
+        total_reviews_span = html.xpath("//span[@class='num']")[0]
 
         if total_reviews_span is None:
             raise FailToGetTotalPageCountError(book_name=bid)
 
-        total_reviews = int(total_reviews_span.search('({}건)')[0].replace(',', ''))
+        try:
+            total_reviews = int(total_reviews_span.search('({}건)')[0].replace(',', ''))
+        except ValueError:
+            raise FailToGetTotalPageCountError(book_name=bid)
 
-        if total_reviews == 0:
-            raise NotExistReviewsError(book_name=bid)
-
-        total_page_num = math.ceil(total_reviews / 10)
-
-        del total_reviews_span
-        return total_page_num
-
-    def set_logger(self, logger):
-        self.logger = logger
+        return total_reviews
 
     def start(self, start_url='http://book.naver.com'):
-        self.update_scraping_bids_from(start_url)
+        response = self.session.get(start_url, headers=self.headers)
+        self.update_scraping_bids_in(response.html)
+        del response
 
         while True:
             bid = self.scraping_bid_sets.pop()
@@ -106,8 +105,7 @@ class NaverBooksScraper:
                 break
 
 
-if __name__ == '__main__':
-    naverbook_logger = logging.getLogger('Naver Books scraper logger')
+def prepare(logger):
     formatter = logging.Formatter('asctime : %(asctime)s '
                                   'funcName : %(funcName)s, '
                                   'message : %(message)s '
@@ -115,13 +113,18 @@ if __name__ == '__main__':
 
     file_handler = logging.FileHandler('naver.log')
     file_handler.setFormatter(formatter)
-    naverbook_logger.addHandler(file_handler)
-    naverbook_logger.setLevel(logging.INFO)
+    logger.addHandler(file_handler)
+    logger.setLevel(logging.INFO)
 
-    naverbook_scraper = NaverBooksScraper()
-    naverbook_scraper.logger = naverbook_logger
+
+if __name__ == '__main__':
+    logger = logging.getLogger('Naver Books scraper logger')
+    prepare(logger)
 
     try:
-        naverbook_scraper.start()
+        scraper = NaverBooksScraper(logger=logger)
+        scraper.start()
     except Exception as e:
-        naverbook_logger.error(e)
+        logger.error(e)
+
+
