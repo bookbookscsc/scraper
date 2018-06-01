861c57ea6960b0651537122c9404462d8fbe27ac
minomi <5minhominho@gmail.com>
2018-06-01 15:14:52 +0900

    store -> bookstore 라고 더 명확하게 변경,  reason 이라는 필드를 추가해서 어떤 Exception 으로 인해 스크래핑이 실패됬는지 출력

--- exceptions.py
+++ exceptions.py
@@ -1,12 +1,21 @@
 class BookScrapingError(Exception):
-    def __init__(self, store, isbn):
-        self.store = store
+    def __init__(self, bookstore, isbn, reason=None):
+        self.bookstore = bookstore
         self.isbn = isbn
+        self.reason = reason
+
+    def __str__(self):
+        return f"Because of {self.reason}, In {self.bookstore}"
 
 
 class FindBookIDError(BookScrapingError):
     def __str__(self):
-        return f"Failed to find book id with {self.isbn} in {self.store}"
+        return super(FindBookIDError, self).__str__() + f"Fail to find book id with {self.isbn}"
+
+
+class ScrapeReviewContentsError(BookScrapingError):
+    def __str__(self):
+        return super(ScrapeReviewContentsError, self).__str__() + f"Fail to Scrape Review Contents of {self.isbn}"
 
 
 class BookLogDetailPopupNotOpenError(BookScrapingError):
@@ -16,14 +25,14 @@ class BookLogDetailPopupNotOpenError(BookScrapingError):
 
 class PaginationError(BookScrapingError):
     def __str__(self):
-        return f"{self.book_name}, 올바른 페이지 범위가 아닙니다."
+        return f"올바른 페이지 범위가 아닙니다."
 
 
 class NotExistReviewsError(BookScrapingError):
     def __str__(self):
-        return f"{self.book_name} 의 리뷰가 없습니다."
+        return f"리뷰가 없습니다."
 
 
 class FailToGetTotalPageCountError(BookScrapingError):
     def __str__(self):
-        return f"{self.book_name} 의 총 리뷰 개수를 구하는데 실패했습니다."
+        return f"총 리뷰 개수를 구하는데 실패했습니다."
