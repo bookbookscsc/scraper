8a92d241207e5a5cf387d85a2bdea48c02bcd59b
minomi <5minhominho@gmail.com>
2018-06-03 06:12:23 +0900

    사용하지 않는 예외 정리

--- exceptions.py
+++ exceptions.py
@@ -16,23 +16,3 @@ class FindBookIDError(BookScrapingError):
 class ScrapeReviewContentsError(BookScrapingError):
     def __str__(self):
         return super(ScrapeReviewContentsError, self).__str__() + f"Fail to Scrape Review Contents of {self.isbn}"
-
-
-class BookLogDetailPopupNotOpenError(BookScrapingError):
-    def __str__(self):
-        return "북 로그 상세 페이지 팝업을 여는데 실패 했습니다."
-
-
-class PaginationError(BookScrapingError):
-    def __str__(self):
-        return f"올바른 페이지 범위가 아닙니다."
-
-
-class NotExistReviewsError(BookScrapingError):
-    def __str__(self):
-        return f"리뷰가 없습니다."
-
-
-class FailToGetTotalPageCountError(BookScrapingError):
-    def __str__(self):
-        return f"총 리뷰 개수를 구하는데 실패했습니다."
