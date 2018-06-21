87f3254f7a5d49dfd61e62e78f8652883fe250ed
minomi <5minhominho@gmail.com>
2018-06-21 18:57:29 +0900

    isbn -> isbn13

--- tests/ScraperTests.py
+++ tests/ScraperTests.py
@@ -1,29 +1,31 @@
 import unittest
 from book_review_scraper import scraper
 from book_review_scraper.bookstores import Naverbook, Kyobo
-from book_review_scraper.exceptions import FindBookIDError
+from book_review_scraper.exceptions import ISBNError
 
 
 class ScraperTests(unittest.TestCase):
 
     def test_scraper_naverbook_meta_class(self):
-        for review in scraper.get_reviews(isbn=9788932919126, bookstores=Naverbook, count=1):
+        for review in scraper.get_reviews(isbn13=9788932919126, bookstores=Naverbook, count=1):
             self.assertIsInstance(review, Naverbook.Review)
 
     def test_scraper_naverbook_iterable(self):
-        for review in scraper.get_reviews(isbn=9788932919126, bookstores=[Naverbook], count=1):
+        for review in scraper.get_reviews(isbn13=9788932919126, bookstores=[Naverbook], count=1):
             self.assertIsInstance(review, Naverbook.Review)
 
     def test_scraper_kyobo_meta_class(self):
-        for review in scraper.get_reviews(isbn=9788932919126, bookstores=Kyobo, count=1):
+        for review in scraper.get_reviews(isbn13=9788932919126, bookstores=Kyobo, count=1):
             self.assertIsInstance(review, Kyobo.Review)
 
     def test_scraper_kyobo_iterable(self):
-        for review in scraper.get_reviews(isbn=9788932919126, bookstores=[Kyobo], count=1):
+        for review in scraper.get_reviews(isbn13=9788932919126, bookstores=[Kyobo], count=1):
             self.assertIsInstance(review, Kyobo.Review)
 
-    def test_scraper_multiple_bookstroe(self):
-        for review in scraper.get_reviews(isbn=9788932919126, bookstores=(Naverbook, Kyobo), count=1):
-            print(review)
+    def test_scraper_multiple_bookstore(self):
+        for review in scraper.get_reviews(isbn13=9788932919126, bookstores=(Naverbook, Kyobo), count=1):
             self.assertTrue(isinstance(review, Naverbook.Review) or isinstance(review, Kyobo.Review))
 
+    def test_scraper_ISBNError(self):
+        gen = scraper.get_reviews(isbn13='wjenwj')
+        self.assertRaises(ISBNError, next, gen)
